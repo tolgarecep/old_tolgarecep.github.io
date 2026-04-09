9990d7b98c49df26dc5b730a690fa7d1fbafb100
tolgarecep <tolgarecepucar@gmail.com>
2026-04-09 18:18:32 +0000

    Deploying to gh-pages from @ tolgarecep/tolgarecep.github.io@8048fbe9e369bfa6f19af69745417b037a97ab3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,8 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-koc-university-science-day",
+          title: 'Koc University Science Day',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
