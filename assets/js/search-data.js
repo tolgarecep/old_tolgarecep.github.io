62863ab5a6a0b0141738d6ed35b71b9b7b9cce2a
tolgarecep <tolgarecepucar@gmail.com>
2026-04-09 14:54:32 +0000

    Deploying to gh-pages from @ tolgarecep/tolgarecep.github.io@19d09dc51bf76e7942948ee62f079292a3d7a90d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,18 +384,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-i-will-be-presenting-at-koc-university-science-day",
           title: 'I will be presenting at Koc University Science Day!',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-i-will-be-presenting-at-koc-university-science-day",
+          title: 'I will be presenting at Koc University Science Day!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
