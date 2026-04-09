c2fc8dcea95bbd01a7fc9acb93197b4ee0ea991d
tolgarecep <tolgarecepucar@gmail.com>
2026-04-09 17:37:09 +0000

    Deploying to gh-pages from @ tolgarecep/tolgarecep.github.io@37310b283b0ee0d6ba5c765b720815432d5934a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-people",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
           section: "Navigation",
